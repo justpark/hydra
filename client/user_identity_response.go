0f66c8725ab89dff804cf368785fab24b9aa4b75
Dan Ursu <10077734@brookes.ac.uk>
2019-06-28 12:04:39 +0100

    Update the user response

--- client/user_identity_response.go
+++ client/user_identity_response.go
@@ -1,5 +1,9 @@
 package client
 
 type UserIdentityResponse struct {
-	UserId string `json:"user_id"`
+	UserId    string `json:"id"`
+	Name      string `json:"name"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	IsManaged bool   `json:"is_managed"`
 }
